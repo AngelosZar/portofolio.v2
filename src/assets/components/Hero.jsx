1bcd47438775c35ac51c77537e04fdf8be338fd7
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-05-30 20:23:07 +0200

    adding some skills to later populate with icons

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,6 +1,6 @@
 function Hero() {
   return (
-    <section>
+    <section className="w-full max-w-[1920px] flex flex-col mx-auto flex-grow min-h-screen">
       <h1> i am the hero</h1>
     </section>
   );
---
+++ src/assets/components/Skills.jsx
@@ -0,0 +1,23 @@
+function Skills() {
+  return (
+    <div>
+      <h2>Skills</h2>
+      <h3>A bunch of icons </h3>
+      <ul>
+        <li>Figma</li>
+        <li>Html</li>
+        <li>css</li>
+        <li>tailwind css</li>
+        <li>Sass</li>
+        <li>Git</li>
+        <li>Js</li>
+        <li>React</li>
+        <li>Jest</li>
+        <li>Cypress</li>
+        <li>HoppScotch</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Skills;
